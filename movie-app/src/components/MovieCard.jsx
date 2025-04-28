90fc6dac992ca638e5407ed8e021f76b5321496d
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-04-28 23:19:02 +0200

    Add MovieCard component and .hintrc configuration

---
+++ movie-app/src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+
+
+function MovieCard ({movie}){
+
+function onFavoriteClick(){
+alert('Clicked');
+}
+
+
+return(
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-over">
+                <button className="favorite-btn" onClick={onFavoriteClick}>🤍</button>
+            </div>
+        </div>
+        <div className="movie-">
+            <h3>{movie.title}</h3>
+           <p>{movie.relase_date}</p> 
+        </div>
+    </div>
+)
+}
+export default MovieCard;
